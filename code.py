c0ea0015e14115c8f3a90fa10aaebdede7e914cf
Billfred Leverette <44166106+Billfred@users.noreply.github.com>
2022-09-22 08:46:27 -0400

    Create code.py

---
+++ code.py
@@ -0,0 +1,29 @@
+# SPDX-FileCopyrightText: 2021 Kattni Rembor for Adafruit Industries
+# SPDX-License-Identifier: MIT
+"""
+NeoSlider NeoPixel Rainbow Demo
+"""
+import board
+import neopixel
+from rainbowio import colorwheel
+from adafruit_seesaw.seesaw import Seesaw
+from adafruit_seesaw.analoginput import AnalogInput
+from adafruit_seesaw import neopixel
+
+# NeoSlider Setup
+neoslider = Seesaw(board.STEMMA_I2C(), 0x30)
+potentiometer = AnalogInput(neoslider, 18)
+pixels = neopixel.NeoPixel(neoslider, 14, 4, pixel_order=neopixel.GRB)
+# boardpixel = neopixel.NeoPixel(board.NEOPIXEL, 1, brightness=0.3)
+
+
+def potentiometer_to_color(value):
+    """Scale the potentiometer values (0-1023) to the colorwheel values (0-255)."""
+    return value / 1023 * 255
+
+
+while True:
+    print(potentiometer.value)
+    # Fill the pixels a color based on the position of the potentiometer.
+    pixels.fill(colorwheel(potentiometer_to_color(potentiometer.value)))
+#    boardpixel.fill(colorwheel(potentiometer_to_color(potentiometer.value)))

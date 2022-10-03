dc24dae8d8bb6474a6436a33d613a71ce9de674b
Billfred Leverette <44166106+Billfred@users.noreply.github.com>
2022-10-03 13:33:07 -0400

    Create everything but sliders
    Initial build-out of the code framework, which has been tested.  Have not integrated the NeoSliders, but it works with the current constants and a switch.  Would also love to build in some better logic around the randomizer to make it "more random" (read: eliminate the chance of getting the same animation twice), but baby steps.

--- code.py
+++ code.py
@@ -1,29 +1,119 @@
-# SPDX-FileCopyrightText: 2021 Kattni Rembor for Adafruit Industries
-# SPDX-License-Identifier: MIT
-"""
-NeoSlider NeoPixel Rainbow Demo
-"""
 import board
 import neopixel
-from rainbowio import colorwheel
-from adafruit_seesaw.seesaw import Seesaw
-from adafruit_seesaw.analoginput import AnalogInput
-from adafruit_seesaw import neopixel
+import digitalio
 
-# NeoSlider Setup
-neoslider = Seesaw(board.STEMMA_I2C(), 0x30)
-potentiometer = AnalogInput(neoslider, 18)
-pixels = neopixel.NeoPixel(neoslider, 14, 4, pixel_order=neopixel.GRB)
-# boardpixel = neopixel.NeoPixel(board.NEOPIXEL, 1, brightness=0.3)
+from adafruit_led_animation.animation.sparkle import Sparkle
+from adafruit_led_animation.animation.blink import Blink
+from adafruit_led_animation.animation.colorcycle import ColorCycle
+from adafruit_led_animation.animation.comet import Comet
+from adafruit_led_animation.animation.chase import Chase
+from adafruit_led_animation.animation.rainbow import Rainbow
+from adafruit_led_animation.animation.rainbowchase import RainbowChase
+from adafruit_led_animation.animation.rainbowcomet import RainbowComet
+from adafruit_led_animation.animation.solid import Solid
 
+from adafruit_led_animation.group import AnimationGroup
+from adafruit_led_animation.sequence import AnimationSequence
+from adafruit_led_animation.sequence import AnimateOnce
+from adafruit_led_animation import color
 
-def potentiometer_to_color(value):
-    """Scale the potentiometer values (0-1023) to the colorwheel values (0-255)."""
-    return value / 1023 * 255
+# Defining which pins are doing what on our QT Py RP2040.
+pixel_pinA = board.A0
+pixel_pinB = board.A1
+pixel_pinC = board.A2
 
+switch = digitalio.DigitalInOut(board.TX)
+switch.switch_to_input(pull=digitalio.Pull.UP)
+
+# Update to match the number of NeoPixels you have connected
+pixel_num = 20
+
+# Sets system-level brightness. This is a number between 0 and 1.
+system_brightness = 0.3
+
+# Sets system-level animation speed. Each animation has a constant multiplied by this.
+# 1.0 is normal speed. 0.5 is double speed, 2.0 is half speed, etc.
+system_speed = 1
+
+pixelsA = neopixel.NeoPixel(pixel_pinA, pixel_num, brightness=system_brightness, auto_write=False)
+pixelsB = neopixel.NeoPixel(pixel_pinB, pixel_num, brightness=system_brightness, auto_write=False)
+pixelsC = neopixel.NeoPixel(pixel_pinC, pixel_num, brightness=system_brightness, auto_write=False)
+
+# These variables dictate the garnetcycle animation.  We use the hex codes universally instead of the
+# Adafruit libraries for colors because we want to define garnet to the University of South Carolina's
+# definition.  And then we redefine white so it isn't quite so eye-searingly bright while running.
+garnetcycle_speed = 0.33*system_speed
+garnet = 0x73000a
+white = 0x555555
+
+# This section defines the animations for the first NeoPixel strip.
+
+garnetcycleA = ColorCycle(pixelsA, garnetcycle_speed, colors=(garnet, white, white))
+sparkleA = Sparkle(pixelsA, speed=0.02*system_speed, color=0x73000a, num_sparkles=6)
+cometA = Comet(pixelsA, 0.05*system_speed, garnet, tail_length=5)
+cometrevA = Comet(pixelsA, 0.05*system_speed, garnet, tail_length=5, reverse=True)
+rainbowA = Rainbow(pixelsA, 0.05*system_speed, precompute_rainbow=True)
+rainbowchaseA = RainbowChase(pixelsA, 0.03*system_speed, size=2, spacing=3, reverse=True)
+rainbowcometA = RainbowComet(pixelsA, 0.05*system_speed)
+rainbowcometrevA = RainbowComet(pixelsA, 0.05*system_speed, reverse=True)
+offA = Solid(pixelsA, 1, 0x000000)
+
+# And the second strip. Note that some things are unique, like the reversal of cometB.
+
+garnetcycleB = ColorCycle(pixelsB, garnetcycle_speed, colors=(white, garnet, white))
+sparkleB = Sparkle(pixelsB, speed=0.02*system_speed, color=0x73000a, num_sparkles=6)
+cometB = Comet(pixelsB, 0.05*system_speed, garnet, tail_length=5)
+cometrevB = Comet(pixelsB, 0.05*system_speed, garnet, tail_length=5, reverse=True)
+rainbowB = Rainbow(pixelsB, 0.05*system_speed, precompute_rainbow=True)
+rainbowchaseB = RainbowChase(pixelsB, 0.03*system_speed, size=2, spacing=3)
+rainbowcometB = RainbowComet(pixelsB, 0.05*system_speed)
+rainbowcometrevB = RainbowComet(pixelsB, 0.05*system_speed, reverse=True)
+offB = Solid(pixelsB, 1, 0x000000)
+
+# And the third strip.  Still other things are unique, like the garnetcycle color order.
+
+garnetcycleC = ColorCycle(pixelsC, garnetcycle_speed, colors=(white, white, garnet))
+sparkleC = Sparkle(pixelsC, speed=0.02*system_speed, color=0x73000a, num_sparkles=6)
+cometC = Comet(pixelsC, 0.05*system_speed, garnet, tail_length=5)
+cometrevC = Comet(pixelsC, 0.05*system_speed, garnet, tail_length=5, reverse=True)
+rainbowC = Rainbow(pixelsC, 0.05*system_speed, precompute_rainbow=True)
+rainbowchaseC = RainbowChase(pixelsC, 0.03*system_speed, size=2, spacing=3, reverse=True)
+rainbowcometC = RainbowComet(pixelsC, 0.05*system_speed)
+rainbowcometrevC = RainbowComet(pixelsC, 0.05*system_speed, reverse=True)
+offC = Solid(pixelsC, 1, 0x000000)
+
+# These functions group and sequence animations to keep them neater in the main list.
+
+cometone = AnimationSequence(cometA, cometrevB, cometC, cometrevA, cometB, cometrevC, advance_on_cycle_complete=True)
+    # AnimationSequence will only start the next strip when the first is complete.
+cometall = AnimationGroup(cometA, cometB, cometC)
+    # AnimationGroup has all three running comets at once.
+garnetcycle = AnimationGroup(garnetcycleA, garnetcycleB, garnetcycleC)
+rainbowchaseall = AnimationGroup(rainbowchaseA, rainbowchaseB, rainbowchaseC)
+rainbowall = AnimationGroup(rainbowA, rainbowB, rainbowC)
+rainbowcometone = AnimationSequence(rainbowcometA, rainbowcometrevB, rainbowcometC, rainbowcometrevA, rainbowcometB, rainbowcometrevC, advance_on_cycle_complete=True)
+rainbowcometall = AnimationGroup(rainbowcometA, rainbowcometB, rainbowcometC)
+offall = AnimationGroup(offA, offB, offC)
+
+animations = AnimationSequence(
+    cometone,
+    rainbowchaseall,
+    cometall,
+    garnetcycle,
+    rainbowall,
+    rainbowcometone,
+    rainbowcometall,
+    auto_clear=True,
+)
+
+offall.animate()
 
 while True:
-    print(potentiometer.value)
-    # Fill the pixels a color based on the position of the potentiometer.
-    pixels.fill(colorwheel(potentiometer_to_color(potentiometer.value)))
-#    boardpixel.fill(colorwheel(potentiometer_to_color(potentiometer.value)))
+
+    if switch.value: # While this switch is NOT pressed
+        offall.animate()
+        animations.random()
+        pass
+    else:
+        animations.animate()
+        pass
